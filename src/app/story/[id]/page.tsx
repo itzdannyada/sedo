2015e715dc60ac4da7b58d1c709367ae6c6fad35
$itzdanny <itzdannyada@gmail.com>
2025-06-05 12:05:54 +0100

    refactor: convert StoryPage to a functional component and adjust parameter handling

--- src/app/story/[id]/page.tsx
+++ src/app/story/[id]/page.tsx
@@ -1,13 +1,17 @@
+import React from "react";
+
 interface PageProps {
     params: { id: string };
 }
 
-export default async function StoryPage({ params }: PageProps) {
-    const { id } = await params;
-    //TODO: Fetch story details using the id
+const StoryPage: React.FC<PageProps> = ({ params }) => {
+    const { id } = params;
+    // TODO: Fetch story details using the id
     return (
         <main>
             <h1>Story ID: {id}</h1>
         </main>
     );
-}
+};
+
+export default StoryPage;
