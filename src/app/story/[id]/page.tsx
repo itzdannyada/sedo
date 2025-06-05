2a075302ac4bcf9503e9440ff499490b8d2d1e15
$itzdanny <itzdannyada@gmail.com>
2025-06-05 12:15:22 +0100

    refactor: convert StoryPage to async function and update parameter handling

--- src/app/story/[id]/page.tsx
+++ src/app/story/[id]/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-const StoryPage = ({ params }: { params: { id: string } }) => {
-    const { id } = params;
-    // TODO: Fetch story details using the id
-    return (
-        <main>
-            <h1>Story ID: {id}</h1>
-        </main>
-    );
-};
-
-export default StoryPage;
+export default async function StoryPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params;
+  // TODO: Fetch story details using the id
+  return (
+    <main>
+      <h1>Story ID: {id}</h1>
+    </main>
+  );
+}
